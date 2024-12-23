a15a9a462b2291b9607ae78a3c4c771d2d0999e9
MarioImpicciche92 <mario.impicciche@gmail.com>
2024-12-23 08:44:38 +0100

    create a form control

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
+  
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+password: string = '';
+email: string = '';
+  
+onSubmit(formData: NgForm){
+    console.log(this.email);
+    console.log(this.password);
+    console.log(formData.form)
+  }
+}
